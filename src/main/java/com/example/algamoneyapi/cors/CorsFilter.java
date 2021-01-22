dc42558dd110f475f7babf962e1888ffe96d4192
AntoniodaHora2102 <antonio_dahora@hotmail.com>
2021-01-22 06:52:11 -0300

    retirada de tempo token

--- src/main/java/com/example/algamoneyapi/cors/CorsFilter.java
+++ src/main/java/com/example/algamoneyapi/cors/CorsFilter.java
@@ -42,7 +42,7 @@ public class CorsFilter implements Filter {
 			//setando as respostas do Headers
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
         	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-        	response.setHeader("Access-Control-Max-Age", "0"); //3600
+        	response.setHeader("Access-Control-Max-Age", "3600"); //3600
 			
 			response.setStatus(HttpServletResponse.SC_OK);
 			
