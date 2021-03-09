9cde59c1f7cb2d23e8fea53ded5d5fd35fa3cf45
AntoniodaHora2102 <antonio_dahora@hotmail.com>
2021-03-08 22:48:04 -0300

    senha usuario antonio

--- src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CategoriaResource.java
@@ -11,6 +11,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.example.algamoneyapi.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin("http://localhost:4200")
 public class CategoriaResource {
 
 	@Autowired
